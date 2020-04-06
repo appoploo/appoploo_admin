85f70808d5bc4735f4e4b950728f3f3a242ddb44
mpoapostolis <mpoapostolis@gmail.com>
2020-04-06 08:21:15 +0300

    edit/new

--- src/routes/Vessels/NewVessel/index.tsx
+++ src/routes/Vessels/NewVessel/index.tsx
@@ -1,77 +1,139 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { Grid, Button } from '@material-ui/core';
-import Details from './Details';
+import {
+  Grid,
+  Button,
+  Card,
+  CardHeader,
+  Divider,
+  CardContent,
+  Theme,
+  colors,
+  TextField,
+  InputAdornment,
+  MenuItem
+} from '@material-ui/core';
 import ActionHeader from '../../../components/ActionHeader';
 import I18n from '../../../I18n';
-import useApi from '../../../Hooks';
-import { useParams, useHistory } from 'react-router';
-import { toast } from 'react-toastify';
+import { makeStyles } from '@material-ui/styles';
+import * as R from 'ramda';
 
-function NewProduct() {
+type vesselType = 'Motor Yacht' | 'Sailing Yacht' | 'Catamaran' | 'Power boat';
+
+const mapVesselTypeToValue: Record<vesselType, string> = {
+  'Motor Yacht':
+    'http://server.cruiser.gr:8290/Appoploo2/api/rest/vesselTypes/1',
+  'Sailing Yacht':
+    'http://server.cruiser.gr:8290/Appoploo2/api/rest/vesselTypes/2',
+  Catamaran: 'http://server.cruiser.gr:8290/Appoploo2/api/rest/vesselTypes/3',
+  'Power boat': 'http://server.cruiser.gr:8290/Appoploo2/api/rest/vesselTypes/4'
+};
+const vesselTypes = Object.keys(mapVesselTypeToValue).map((k) => ({
+  label: k,
+  value: mapVesselTypeToValue[k as vesselType]
+}));
+
+const useStyles = makeStyles((theme: Theme) => ({
+  cardContent: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap',
+    minHeight: '167px',
+    maxHeight: '61vh',
+    overflowY: 'auto'
+  }
+}));
+
+function NewVessel() {
   const t = useContext(I18n);
-  type Image = {
-    file: File;
-    url: string;
-  };
-  const [images, setImages] = useState<Image[]>([]);
-  const [infos, setInfos] = useState<{
-    name: string;
-    price: number;
-    lpReward: number;
-  }>({
+  const classes = useStyles();
+  const [state, setState] = useState({
     name: '',
-    price: 0,
-    lpReward: 0
+    vesselType: '',
+    description: '',
+    loa: 0,
+    draught: 0
   });
-
-  const api = useApi();
-  const params = useParams<{ id?: string }>();
-
-  const deleteImages = (paths: string[]) => {
-    api
-      .delete(`/api/bo/products/${params.id}/images`, {
-        json: {
-          paths
-        }
-      })
-      .then(d => d.json());
-  };
-
-  const isEdit = Boolean(params.id);
-  const history = useHistory();
-  const handleSubmit = () => {
-    const formData = new FormData();
-
-    images.filter(f => f.file).forEach(f => formData.append('image', f.file));
-    formData.append('infos', JSON.stringify(infos));
-    const action = isEdit ? api.put : api.post;
-    const url = isEdit ? `/api/bo/products/${params.id}` : `/api/bo/products`;
-    const successMsg = isEdit
-      ? 'int.product-updated-successfully'
-      : 'int.product-created-successfully';
-    action(url, {
-      body: formData
-    }).then(() => {
-      toast.success(successMsg);
-      history.push('/products');
-    });
-  };
-
   return (
     <>
       <ActionHeader>
-        <Button onClick={handleSubmit} variant="outlined">
+        <Button onClick={() => void 0} variant="outlined">
           {t('int.save')}
         </Button>
       </ActionHeader>
       <br />
       <Grid container spacing={4}>
         <Grid item xl={6} lg={6} md={12} xs={12}>
-          <Details infos={infos} setInfos={setInfos} />
+          <Card>
+            <CardHeader
+              subheader={t('int.edit-vessel-info')}
+              title={t('int.vessel')}></CardHeader>
+            <Divider />
+            <CardContent className={classes.cardContent}>
+              <Grid container spacing={2}>
+                <Grid item md={12} xs={12}>
+                  <TextField
+                    fullWidth
+                    label={t('int.vessel-name')}
+                    margin="dense"
+                    required
+                    variant="outlined"
+                  />
+                </Grid>
+
+                <Grid item md={12} xs={12}>
+                  <TextField
+                    select
+                    fullWidth
+                    label={t('int.vessel-type')}
+                    margin="dense"
+                    required
+                    variant="outlined">
+                    {vesselTypes.map((obj, idx) => (
+                      <MenuItem key={idx} value={obj.value}>
+                        {obj.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                </Grid>
+
+                <Grid item md={12} xs={12}>
+                  <TextField
+                    fullWidth
+                    label={t('Vessel description')}
+                    required
+                    margin="dense"
+                    variant="outlined"
+                  />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label={t('int.description')}
+                    required
+                    type="number"
+                    margin="dense"
+                    variant="outlined"
+                  />
+                </Grid>
+
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    type="number"
+                    label={t('Length (LOA)')}
+                    required
+                    margin="dense"
+                    variant="outlined"
+                  />
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
         </Grid>
       </Grid>
     </>
   );
 }
 
-export default NewProduct;
+export default NewVessel;
